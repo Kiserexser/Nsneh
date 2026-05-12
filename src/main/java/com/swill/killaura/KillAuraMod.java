4031c67db3a21227c78420cb800a821676da64f1
Kiserexser <towartors@mail.ru>
2026-05-12 22:27:42 +0500

    Create KillAuraMod.java

---
+++ src/main/java/com/swill/killaura/KillAuraMod.java
@@ -0,0 +1,10 @@
+package com.swill.killaura;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KillAuraMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("[KillAura] Мод активирован!");
+    }
+}
